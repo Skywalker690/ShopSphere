2185febe3ae94d15d67e6de4eb05ad5c572a354b
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-05 12:14:37 +0530

    feat: create Coupon entity with id field

--- backend/src/main/java/com/sanjo/ecommerce/model/Coupon.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/Coupon.java
@@ -1,4 +1,22 @@
 package com.sanjo.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "coupon")
 public class Coupon {
+    @Id
+    private Long id;
+
+
 }
