ddddb30c74ef2428cecc147ef216c262fe3dedab
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-05 14:17:07 +0530

    Create Product

---
+++ backend/src/main/java/com/sanjo/ecommerce/model/Product.java
@@ -0,0 +1,19 @@
+package com.sanjo.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
