5b6256315cdfc45797034467541e7ff3318e99a4
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-05 14:27:43 +0530

    Create Seller

---
+++ backend/src/main/java/com/sanjo/ecommerce/model/Seller.java
@@ -0,0 +1,19 @@
+package com.sanjo.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "seller")
+public class Seller {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
