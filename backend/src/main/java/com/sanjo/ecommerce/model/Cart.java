c57ce793b7763d0d4dd0cd2b3ec659bf89f652c4
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-05 13:08:57 +0530

    feat: add Cart entity with user, items, pricing, and coupon details

---
+++ backend/src/main/java/com/sanjo/ecommerce/model/Cart.java
@@ -0,0 +1,39 @@
+package com.sanjo.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "cart")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    private Set<CartItem> cartItems = new HashSet<>();
+
+    private double totalSellingPrice;
+
+    private int item;
+
+    private int totalMRP;
+
+    private int discount;
+
+    private String couponCode;
+}
