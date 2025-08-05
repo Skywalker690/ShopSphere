57e3df77743ff5e183577abf0bc20af792f1ecd6
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-05 12:14:17 +0530

    feat: add Address entity with basic location and contact fields

--- backend/src/main/java/com/sanjo/ecommerce/model/Address.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/Address.java
@@ -1,4 +1,27 @@
 package com.sanjo.ecommerce.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "address")
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String  name;
+    private String  locality;
+    private String  address;
+    private String  city;
+    private String  state;
+    private String  pinCode;
+    private String  mobile;
 }
