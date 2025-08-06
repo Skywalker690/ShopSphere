043e5496b2ed83ca08f185c7afc20b6815886ae5
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-06 20:17:14 +0530

    feat: add Seller entity with embedded business, bank, and address info

--- backend/src/main/java/com/sanjo/ecommerce/model/Seller.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/Seller.java
@@ -1,5 +1,7 @@
 package com.sanjo.ecommerce.model;
 
+import com.sanjo.ecommerce.domain.AccountStatus;
+import com.sanjo.ecommerce.domain.USER_ROLE;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,4 +18,32 @@ public class Seller {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String sellerName;
+
+    private String mobile;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    @Embedded
+    private BusinessDetails businessDetails = new BusinessDetails();
+
+    @Embedded
+    private BankDetails bankDetails = new BankDetails();
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address pickupAddress = new Address();
+
+    private String GSTIN;
+
+    private USER_ROLE role = USER_ROLE.ROLE_SELLER;
+
+    private boolean isEmailVerified = false;
+
+    private AccountStatus accountStatus = AccountStatus.PENDING_VERIFICATION;
+
+
 }
