532c29727dcf22e99d4be9247dcbc9e6002c822c
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-06 18:33:45 +0530

    refactor: update Cart entity with mapped CartItem relationship and cascade settings

--- backend/src/main/java/com/sanjo/ecommerce/model/Cart.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/Cart.java
@@ -25,6 +25,11 @@ public class Cart {
     @OneToOne
     private User user;
 
+    @OneToMany(
+            mappedBy = "cart",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
     private Set<CartItem> cartItems = new HashSet<>();
 
     private double totalSellingPrice;
