f8f0cd3b292e3de2411c725b3a139b51c8e43520
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-06 18:27:46 +0530

    feat: add CartItem entity with pricing, quantity, and relationships

--- backend/src/main/java/com/sanjo/ecommerce/model/CartItem.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/CartItem.java
@@ -1,5 +1,6 @@
 package com.sanjo.ecommerce.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,4 +17,22 @@ public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private int quantity =1;
+
+    private String size;
+
+    private Integer mprPrice;
+
+    private Integer sellingPrice;
+
+    private Long userId;
+
+    @ManyToOne
+    @JsonIgnore
+    private Cart cart;
+
+    @OneToOne
+    private Product product;
+
 }
