567293c7add5f18d5c7661d2c54c81b64401c9be
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-08 21:18:29 +0530

    feat: add OrderItem entity with product, pricing, and order reference

--- backend/src/main/java/com/sanjo/ecommerce/model/OrderItem.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/OrderItem.java
@@ -1,4 +1,36 @@
 package com.sanjo.ecommerce.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    private Order order;
+
+    @ManyToOne
+    private Product product;
+
+    private String size;
+
+    private int quantity;
+
+    private Integer mrpPrice;
+
+    private Integer sellingPrice;
+
+    private Long userId;
 }
