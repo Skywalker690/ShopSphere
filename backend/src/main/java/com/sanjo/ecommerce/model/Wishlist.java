0f8901c110c39c9ed411be1a3d05969a643d9a9e
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-08 21:42:12 +0530

    feat: add Wishlist entity with user and product associations

--- backend/src/main/java/com/sanjo/ecommerce/model/Wishlist.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/Wishlist.java
@@ -1,4 +1,28 @@
 package com.sanjo.ecommerce.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Wishlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private User user;
+
+    @ManyToMany
+    private Set<Product> products = new HashSet<>();
 }
