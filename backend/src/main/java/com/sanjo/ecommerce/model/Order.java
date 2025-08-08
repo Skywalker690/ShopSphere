2e1f1bf0f8607408bfbdf51e838740cd2cb0645f
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-08 21:06:42 +0530

    feat: add Order entity with user, items, address, payment, and status tracking

--- backend/src/main/java/com/sanjo/ecommerce/model/Order.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/Order.java
@@ -1,4 +1,64 @@
 package com.sanjo.ecommerce.model;
 
+import com.sanjo.ecommerce.domain.OrderStatus;
+import com.sanjo.ecommerce.domain.PaymentStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Table(name = "order")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderId;
+
+    @ManyToOne
+    private User user;
+
+    private Long sellerId;
+
+    @OneToMany(
+            mappedBy = "order",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @ManyToOne
+    private Address shippingAddress;
+
+    @Embedded
+    private PaymentDetails paymentDetails = new PaymentDetails();
+
+    private double totalMrpPrice;
+
+    private Integer totalSellingPrice;
+
+    private Integer discount;
+
+    private OrderStatus orderStatus;
+
+    private int totalItem;
+
+    private PaymentStatus paymentStatus = PaymentStatus.PENDING;
+
+    private LocalDate orderDate = LocalDate.now();
+    private LocalDate deliverDate = orderDate.plusDays(7);
+
+
+
+
 }
