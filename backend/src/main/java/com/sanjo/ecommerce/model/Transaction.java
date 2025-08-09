d1664d97e8782840f0b3466f5a02cdb473c23c5c
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-09 17:40:55 +0530

    feat: add Transaction entity linking customer, order, and seller

--- backend/src/main/java/com/sanjo/ecommerce/model/Transaction.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/Transaction.java
@@ -1,4 +1,31 @@
 package com.sanjo.ecommerce.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User customer;
+
+    @OneToOne
+    private Order order;
+
+    @ManyToOne
+    private Seller seller;
+
+    private LocalDate date = LocalDate.now();
 }
