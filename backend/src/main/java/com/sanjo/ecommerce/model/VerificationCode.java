49c6bb2feb4f0c3212df015d6a394875d5c67ee8
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-09 17:44:10 +0530

    feat: add VerificationCode entity for OTP-based user and seller verification

--- backend/src/main/java/com/sanjo/ecommerce/model/VerificationCode.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/VerificationCode.java
@@ -1,4 +1,28 @@
 package com.sanjo.ecommerce.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class VerificationCode {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String otp;
+
+    private String email;
+
+    @OneToOne
+    private User user;
+
+    @OneToOne
+    private Seller seller;
 }
