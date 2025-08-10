db06e84fddce8dbb18d1dc9297dff4b72bbc200a
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-10 20:46:39 +0530

    feat: implement JwtTokenValidator filter skeleton to extract Authorization header

--- backend/src/main/java/com/sanjo/ecommerce/config/JwtTokenValidator.java
+++ backend/src/main/java/com/sanjo/ecommerce/config/JwtTokenValidator.java
@@ -1,12 +1,17 @@
 package com.sanjo.ecommerce.config;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-public class JwtTokenValidator implements Filter {
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+public class JwtTokenValidator extends OncePerRequestFilter {
 
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt = request.getHeader("Authorization");
     }
 }
