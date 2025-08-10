daf03507942ce0321154d42affcdc2e6f9c2f9c5
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-10 20:01:41 +0530

    chore: add JwtTokenValidator filter skeleton for JWT validation

---
+++ backend/src/main/java/com/sanjo/ecommerce/config/JwtTokenValidator.java
@@ -0,0 +1,12 @@
+package com.sanjo.ecommerce.config;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class JwtTokenValidator implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+}
