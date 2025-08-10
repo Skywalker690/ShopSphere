be7661031540b4f6e51a5b1bece6f56df6eed8f1
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-10 12:18:33 +0530

    feat: add PaymentOrder entity with amount, status, method, and linked orders

--- backend/src/main/java/com/sanjo/ecommerce/model/PaymentOrder.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/PaymentOrder.java
@@ -1,4 +1,36 @@
 package com.sanjo.ecommerce.model;
 
+import com.sanjo.ecommerce.domain.PaymentMethod;
+import com.sanjo.ecommerce.domain.PaymentOrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class PaymentOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long amount;
+
+    private PaymentOrderStatus status = PaymentOrderStatus.PENDING;
+
+    private PaymentMethod paymentMethod;
+
+    private String paymentLinkId;
+
+    @ManyToOne
+    private User user;
+
+    @OneToMany
+    private Set<Order> orders = new HashSet<>();
+
 }
