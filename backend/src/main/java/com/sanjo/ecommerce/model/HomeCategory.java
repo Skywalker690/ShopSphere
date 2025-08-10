67e7e5f8aa64969551fa93a521932b43940797ed
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-10 12:38:01 +0530

    feat: add HomeCategory entity with name, image, categoryId, and section

--- backend/src/main/java/com/sanjo/ecommerce/model/HomeCategory.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/HomeCategory.java
@@ -1,4 +1,27 @@
 package com.sanjo.ecommerce.model;
 
+import com.sanjo.ecommerce.domain.HomeCategorySection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class HomeCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String image;
+    private String categoryId;
+    private HomeCategorySection section;
 }
