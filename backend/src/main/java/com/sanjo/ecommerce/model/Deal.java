1681cb0b89f6e66c6f3481089ec3dc89d0e755cd
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-10 17:17:40 +0530

    feat: add Deal entity with discount and linked HomeCategory

--- backend/src/main/java/com/sanjo/ecommerce/model/Deal.java
+++ backend/src/main/java/com/sanjo/ecommerce/model/Deal.java
@@ -1,4 +1,24 @@
 package com.sanjo.ecommerce.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Deal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer discount;
+
+    @OneToOne
+    private HomeCategory category;
+
 }
