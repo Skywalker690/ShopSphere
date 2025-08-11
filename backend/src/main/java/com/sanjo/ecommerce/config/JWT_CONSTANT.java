683b719d3a423c4910cd1d876d6b7455fcaae709
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-11 18:18:47 +0530

    chore: add JWT_CONSTANT class for storing secret key and header name

---
+++ backend/src/main/java/com/sanjo/ecommerce/config/JWT_CONSTANT.java
@@ -0,0 +1,8 @@
+package com.sanjo.ecommerce.config;
+
+
+public class JWT_CONSTANT {
+
+    public static final String SECRET_KEY ="skdsiaoidjasodijrirugvnmdofisodmsdcojsdi";
+    public static final String JWT_HEADER ="Authorization";
+}
