24279df7f81705a92c368cf176b45ef5c805de7c
sanjoksiby <sibykchacko02322@gmail.com>
2025-08-13 20:19:18 +0530

    Refactor

--- backend/src/main/java/com/sanjo/ecommerce/config/JwtProvider.java
+++ backend/src/main/java/com/sanjo/ecommerce/config/JwtProvider.java
@@ -1,9 +1,11 @@
 package com.sanjo.ecommerce.config;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Collection;
@@ -11,6 +13,7 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class JwtProvider{
 
     SecretKey key = Keys.hmacShaKeyFor(JWT_CONSTANT.SECRET_KEY.getBytes());
@@ -21,11 +24,19 @@ public class JwtProvider{
 
         return Jwts.builder()
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime()+86400000))
+                .setExpiration(new Date(new Date().getTime()+86400000)) //24Hr
                 .claim("email",auth.getName())
                 .claim("authorities",roles)
                 .signWith(key)
                 .compact();
+    }
+
+    public String getEmailFromJwtToken(String jwt){
+        jwt = jwt.substring(7);
+        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().
+                parseClaimsJws(jwt).getBody();
+
+        return String.valueOf(claims.get("email"));
     }
 
     private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
--- backend/src/main/java/com/sanjo/ecommerce/controller/AuthController.java
+++ backend/src/main/java/com/sanjo/ecommerce/controller/AuthController.java
@@ -1,4 +1,10 @@
 package com.sanjo.ecommerce.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class AuthController {
+
+
 }
