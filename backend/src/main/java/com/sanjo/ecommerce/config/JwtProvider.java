88cfeb627c03b1cdec3bdd148f717600e9dab139
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-15 14:54:30 +0530

    implemented JWT Provider

--- backend/src/main/java/com/sanjo/ecommerce/config/JwtProvider.java
+++ backend/src/main/java/com/sanjo/ecommerce/config/JwtProvider.java
@@ -24,7 +24,7 @@ public class JwtProvider{
 
         return Jwts.builder()
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime()+86400000)) //24Hr
+                .setExpiration(new Date(new Date().getTime()+86400000)) //24Hr78
                 .claim("email",auth.getName())
                 .claim("authorities",roles)
                 .signWith(key)
@@ -48,3 +48,4 @@ public class JwtProvider{
         return String.join(",",auths);
     }
 }
+
