9b29c842b1ec83cd8f2132b474c102fdbaf94107
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-15 20:58:34 +0530

    implemented CustomUserService for loading users by email with roles

---
+++ backend/src/main/java/com/sanjo/ecommerce/service/impl/CustomUserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.sanjo.ecommerce.service.impl;
+
+import com.sanjo.ecommerce.domain.USER_ROLE;
+import com.sanjo.ecommerce.model.User;
+import com.sanjo.ecommerce.repository.UserRepository;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomUserServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+    private static final String SELLER_PREFIX = "seller_";
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if(username.startsWith(SELLER_PREFIX)){
+
+        }else {
+            User user = userRepository.findByEmail(username);
+            if(user != null){
+                return buildUserDetails(user.getEmail(),user.getPassword(),user.getRole());
+            }
+        }
+        throw new UsernameNotFoundException("User or Seller Not Found "+username);
+    }
+
+    private UserDetails buildUserDetails(@NotBlank(message = "Email is required") String email, String password, USER_ROLE role) {
+        if(role == null) role =USER_ROLE.ROLE_CUSTOMER;
+
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority("Role_" + role));
+
+        return new org.springframework.security.core.userdetails.User(
+                email,
+                password,
+                authorityList
+        );
+    }
+}
