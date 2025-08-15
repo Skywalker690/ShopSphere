12cb2cc8202cede10be751e9af5ede715adafa38
Skywalker690 <sanjoksiby0@gmail.com>
2025-08-15 20:57:57 +0530

    feat: add SellerRepository for Seller entity CRUD operations

---
+++ backend/src/main/java/com/sanjo/ecommerce/repository/SellerRepository.java
@@ -0,0 +1,7 @@
+package com.sanjo.ecommerce.repository;
+
+import com.sanjo.ecommerce.model.Seller;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SellerRepository extends JpaRepository<Seller,Long> {
+}
